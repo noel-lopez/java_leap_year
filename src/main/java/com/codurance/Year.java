5693946a10291df638c68fc12a2117e3ff2d1ff2
Noel Lopez <nlopez.publi@gmail.com>
2024-03-18 17:47:55 +0100

    refactor: create isNotDivisibleBy method

--- src/main/java/com/codurance/Year.java
+++ src/main/java/com/codurance/Year.java
@@ -2,7 +2,7 @@ package com.codurance;
 
 public class Year {
     public static boolean isLeap(int year) {
-        if (isDivisibleBy(year, 100) && !isDivisibleBy(year, 400)) {
+        if (isDivisibleBy(year, 100) && isNotDivisibleBy(year, 400)) {
             return false;
         }
 
@@ -11,5 +11,9 @@ public class Year {
 
     private static boolean isDivisibleBy(int year, int number) {
         return year % number == 0;
+    }
+
+    private static boolean isNotDivisibleBy(int year, int number) {
+        return !isDivisibleBy(year, number);
     }
 }
