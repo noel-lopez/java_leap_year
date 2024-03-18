39a8afea2c9e1a34f5525e5916ed2b016e31dc06
Noel Lopez <nlopez.publi@gmail.com>
2024-03-18 16:45:59 +0100

    refactor: change if condition to years are leap if divisible by 4

--- src/main/java/com/codurance/Year.java
+++ src/main/java/com/codurance/Year.java
@@ -2,7 +2,7 @@ package com.codurance;
 
 public class Year {
     public static boolean isLeap(int year) {
-        if (year == 1996 || year == 1992 || year == 1988) {
+        if (year % 4 == 0) {
             return true;
         }
 
