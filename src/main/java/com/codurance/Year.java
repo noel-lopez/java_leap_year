82125cc78d973b649ea3b03be02a1eec2a367874
Benjamin Rae <benjaminrae93@gmail.com>
2024-03-18 16:39:00 +0100

    refactor: join conditions in same if statement in isLeap

--- src/main/java/com/codurance/Year.java
+++ src/main/java/com/codurance/Year.java
@@ -2,13 +2,10 @@ package com.codurance;
 
 public class Year {
     public static boolean isLeap(int year) {
-        if (year == 1992) {
+        if (year == 1996 || year == 1992) {
             return true;
         }
 
-        if (year == 1996) {
-            return true;
-        }
         return false;
     }
 }
