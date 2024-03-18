d5a1f8d394475f5e6b3bf243d0730088da0f6845
Noel Lopez <nlopez.publi@gmail.com>
2024-03-18 16:33:09 +0100

    feat: 1996 is a leap year

--- src/main/java/com/codurance/Year.java
+++ src/main/java/com/codurance/Year.java
@@ -2,6 +2,9 @@ package com.codurance;
 
 public class Year {
     public static boolean isLeap(int year) {
+        if (year == 1996) {
+            return true;
+        }
         return false;
     }
 }
