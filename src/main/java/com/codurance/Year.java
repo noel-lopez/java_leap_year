35eb9bb898d81535abdd27685636165edc2ec913
Noel Lopez <nlopez.publi@gmail.com>
2024-03-18 16:41:40 +0100

    feat: 1988 is a leap year

--- src/main/java/com/codurance/Year.java
+++ src/main/java/com/codurance/Year.java
@@ -2,7 +2,7 @@ package com.codurance;
 
 public class Year {
     public static boolean isLeap(int year) {
-        if (year == 1996 || year == 1992) {
+        if (year == 1996 || year == 1992 || year == 1988) {
             return true;
         }
 
